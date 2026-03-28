580f4f7273193c0662eb78f519a6338d8d9fdf1f
Avaya Khatri <130426359+kavae@users.noreply.github.com>
2026-03-28 16:37:37 +0545

    change mainsection file name to avoid confusion

--- src/components/layout/Mainsection.jsx
+++ src/components/layout/Mainsection.jsx
@@ -1,11 +1,11 @@
 import { Herosection } from "./Herosection"
 import { Sidebar } from "./Sidebar"
 
-export const Main = () => {
+export const Mainsection = () => {
     return (
         <div className="grid grid-cols-6">
             <Sidebar />
-            <Herosection className="col-span-5" />
+            <Herosection />
         </div>
     )
 }
